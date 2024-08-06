e3248145e5f2ffc8978da71fba05befcfd99ce99
Suman S <113275985+Snugtroller@users.noreply.github.com>
2024-08-07 02:00:42 +0530

    Delete train.py

--- train.py
+++
@@ -1,8 +0,0 @@
-import pickle
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
-from sklearn.model_selection import train_test_split
-import numpy as np
-
-data_dict=pickle.load(open("leaf_data.pickle","rb"))
-print(data_dict)
